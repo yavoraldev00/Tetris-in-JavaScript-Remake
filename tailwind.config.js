26db22a405355da91b53a5a16ef35b21efaabd37
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-11-06 17:32:39 +0100

    Added open-sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "open-font": ["Open Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
